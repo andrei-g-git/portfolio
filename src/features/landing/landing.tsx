1f3ccc975921350ad2cf754cef21923685aedf1e
andrei_g_git <andrei_g_git@protonmail.com>
2024-07-03 18:35:56 +0300

    add test move animation, buggy

--- src/features/landing/landing.tsx
+++ src/features/landing/landing.tsx
@@ -5,11 +5,13 @@ import "./landing.scss";
 import { useEffect, useRef, useState, MouseEvent } from "react";
 
 function Landing(props: any) {
+    const charDiv = document.getElementsByClassName("test-char")[0];
+
     const [state, send] = useMachine(characterMachine);
 
     const charRef = useRef<null | HTMLDivElement>(null);
 
-    const [x, setX] = useState<number>(0);
+    const [x, setX] = useState<number>(charRef.current? charRef.current.offsetLeft: 0);
 
     const [mouseX, setMouseX] = useState<number>(0);
 
@@ -23,9 +25,25 @@ function Landing(props: any) {
         <div className="landing-container">
             <div className="landing-canvas">
                 <div className="test-container"
-                    onClick={(event) => handleClick(event, setMouseX)}
+                    onClick={(event) => handleClick(
+                        charDiv,
+                        mouseX,
+                        event, 
+                        x, 
+                        charRef.current? charRef.current.offsetWidth : 0, 
+                        setMouseX, 
+                        () => {})
+                    }
                 >
-                    <div className="test-char"></div>
+                    <div className="test-char"
+                        ref={charRef}
+                        style={{
+                            position: 'absolute',
+                            left: `calc(${mouseX}px - ${charRef.current? charRef.current.offsetWidth/2 : 0}px)`
+                        }}
+                    >
+
+                    </div>
                     <div className="test-react"></div>
                     <div className="test-nodejs"></div>
                     <div className="test-mysql"></div>                    
@@ -41,12 +59,22 @@ const getX = (ref: React.MutableRefObject<null | HTMLDivElement>) => {
     return ref.current?.offsetLeft;
 }
 
-const handleClick = (event: MouseEvent, setX: React.Dispatch<React.SetStateAction<number>>) => {
-    setX(event.clientX);
+const handleClick = (charDiv: Element, mouseX: number, event: MouseEvent, charX: number, charWidth: number, setMouseX: React.Dispatch<React.SetStateAction<number>>, pickDirection: Function) => {
+    setMouseX(event.clientX);
     console.log("clicked at positio x:   ", event.clientX)
+
+    const delta = event.clientX - (charX + charWidth/2);
+    const isGoingRight = Boolean(Math.max(0, delta));
+    console.log('delta   ', delta)
+    console.log('GOING RIGHT:   ', isGoingRight)
+
+    console.log("MouseX:   ", mouseX)
+
+    charDiv.classList.add("test-move-animation") //gotta remove
+    return isGoingRight;
 }
 
-const getIfGoingRighy = () => {
+const getIfGoingRight = (mouseX: number, charX: number) => {
     
 }
 
